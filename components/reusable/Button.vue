02dcd4414008e5faf062b0e0eeb10663fbd3def9
RivierGrullon <rgrullon@ipisdosaj.edu.do>
2022-03-14 22:42:35 -0400

    First commit

---
+++ components/reusable/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button>{{ title }}</button>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: {
+                type: String,
+                required: true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>>
+
+</style>
---
+++ store/index.js
@@ -0,0 +1,95 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const state = () => ({
+  projectsHeading: "Projects Portfolio",
+  projectsDescription: "Some of the projects I have successfully completed",
+  projects: [
+    {
+      id: "https://hostifyside.netlify.app/",
+      title: "Hostify backend",
+      category: "Web Application",
+      img: "images/Hostify.png",
+      tag: "Backend",
+    },
+    {
+      id: "https://dev-to-nuxt.netlify.app/",
+      title: "Dev.to UI Nuxt",
+      category: "Web Application",
+      img: "images/dev-to-clone.png",
+      tag: "UI / Frontend",
+    },
+  ],
+  aboutMe: [
+    {
+      id: uuidv4(),
+      bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
+    },
+    {
+      id: uuidv4(),
+      bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",
+    },
+  ],
+  copyrightDate: new Date().getFullYear(),
+  socialProfiles: [
+    {
+      id: uuidv4(),
+      name: "GitHub",
+      icon: "github",
+      url: "https://github.com/RivierGrullon",
+    },
+    {
+      id: uuidv4(),
+      name: "Twitter",
+      icon: "twitter",
+      url: "https://twitter.com/RamirezRivier",
+    },
+    {
+      id: uuidv4(),
+      name: "Dev.to",
+      icon: "book",
+      url: "https://dev.to/riviergrullon",
+    },
+    {
+      id: uuidv4(),
+      name: "Linkedin",
+      icon: "linkedin",
+      url: "https://www.linkedin.com/in/rivier-grullon-ba0a25204/",
+    },
+  ],
+  categories: [
+    {
+      id: uuidv4(),
+      value: "web",
+      name: "Web Application",
+    },
+    {
+      id: uuidv4(),
+      value: "mobile",
+      name: "Mobile Applicaiton",
+    },
+    {
+      id: uuidv4(),
+      value: "ui-ux",
+      name: "UI/UX Design",
+    },
+    {
+      id: uuidv4(),
+      value: "branding",
+      name: "Branding & Animations",
+    },
+  ],
+});
+
+export const getters = {
+  getProjectById: (state) => (id) => {
+    return state.projects.find((project) => project.id == id);
+  },
+};
+
+export const mutations = {
+  // @todo
+};
+
+export const actions = {
+  // @todo
+};
